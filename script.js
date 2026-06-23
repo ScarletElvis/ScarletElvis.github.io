a251e42b6135dacbb58b2c36cb61cbc94235e788
Elvis Chimaobi Opara <elvischimaobi11@gmail.com>
2026-06-24 00:52:01 +0100

    Add hamburger menu, smooth scroll, and fix mobile button width

---
+++ script.js
@@ -0,0 +1,4 @@
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-Links");
+
+hamburger.addEventListener("click", function () {navLinks.classList.toggle("open");});
