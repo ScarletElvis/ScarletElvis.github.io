07c58c71c0c4d45d5ee5e4a5bf329313cc90fa0f
Elvis Chimaobi Opara <elvischimaobi11@gmail.com>
2026-06-24 01:22:55 +0100

    Fix hamburger menu js code

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("nav-Links");
+const navLinks = document.getElementById("nav-links");
 
 hamburger.addEventListener("click", function () {navLinks.classList.toggle("open");});
+
+navLinks.querySelectorAll("a").forEach(function (link) {link.addEventListener("click", function () {navLinks.classList.remove("open");
+});
+});
